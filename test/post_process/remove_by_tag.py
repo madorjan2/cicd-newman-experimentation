604a89489d3a8ef3d0e3e5dc174dd1d70df7d95c
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2024-01-26 01:15:33 +0100

    added remove_by_tag.py

---
+++ test/post_process/remove_by_tag.py
@@ -0,0 +1,22 @@
+import os
+
+TAG = 'setup'
+
+def get_file_list():
+	parent_dir = os.path.dirname(os.getcwd())
+	path = os.path.join(parent_dir, 'allure-results')
+	return [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f[-5:] == ".json"]
+
+def select_by_tag():
+	selected = []
+	for f in get_file_list():
+		with open(f, 'r') as jsonfile:
+			if f'"name":"tag","value":"{TAG}"' in jsonfile.read():
+				selected.append(f)
+	return selected
+
+def delete_from(file_list):
+	for f in file_list:
+		os.remove(f)
+
+delete_from(select_by_tag())
